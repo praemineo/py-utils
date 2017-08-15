7bd2ed0648711326832abc345e9d3708b510a62e
Tushar Pawar <tusharvijaypawar@gmail.com>
2017-08-15 22:50:20 +0530

    Mongo utilities.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mongo_utils',
+  packages = ['mongo_utils'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Package for mongo operation. Internal usage in Preeminence.',
+  author = 'Tushar Pawar',
+  author_email = 'tusharvijaypawar@gmail.com',
+  url = 'https://github.com/preeminence/py-utils', # use the URL to the github repo
+  download_url = 'https://github.com/preeminence/py-utils/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['mongo', 'preeminence', 'utility'], # arbitrary keywords
+  classifiers = [],
+)
