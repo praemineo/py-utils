410a3f1c8ab7b4cbc427d8b74b04d2f517d6081b
Tushar Pawar <tusharvijaypawar@gmail.com>
2017-08-15 23:48:54 +0530

    Installing locally

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from distutils.core import setup
-setup(
-  name = 'mongo_utils',
-  packages = ['mongo_utils'], # this must be the same as the name above
-  version = '0.1',
-  description = 'Package for mongo operation. Internal usage in Preeminence.',
-  author = 'Tushar Pawar',
-  author_email = 'tusharvijaypawar@gmail.com',
-  url = 'https://github.com/preeminence/py-utils', # use the URL to the github repo
-  download_url = 'https://github.com/preeminence/py-utils/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['mongo', 'preeminence', 'utility'], # arbitrary keywords
-  install_requires=['pymongo',],
-  classifiers = []
-)
+
+from setuptools import setup
+
+setup(name='preeminence_utils',
+      version='0.1',
+      description='Package for Internal usage in Preeminence.',
+      url='https://github.com/preeminence/py-utils',
+      author='Tushar Pawar',
+      author_email='tusharvijaypawar@gmail.com',
+      license='MIT',
+      packages=['preeminence_utils'],
+      install_requires=[
+          'pymongo',
+      ],
+      zip_safe=False)
