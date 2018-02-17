7281ee52b7b744a24d649b68a254e4efda64f844
Tushar Pawar <tusharvijaypawar@gmail.com>
2018-02-17 15:55:59 +0530

    Remove dependencies in setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='preeminence_utils',
       license='MIT',
       packages=find_packages(),
       install_requires=[
-          'pymongo','tensorflow-gpu','numpy','awscli','boto3'
+          'pymongo'
       ],
       zip_safe=False)
